7efdac8dd8ee687ba0b4f1766ae7d0cf575fc97d
Caio Nemesio <caionemesio2@gmail.com>
2023-12-04 16:05:54 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Projeto_Estagio',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
